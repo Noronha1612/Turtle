3296c74f0b4092ecfe7127614c5db2d7c4557735
NoronhaGCP <inc.691@gmail.com>
2021-01-16 18:11:26 -0300

    test: add fake data to simulate user using faker

---
+++ server/__tests__/utils/createUser.ts
@@ -0,0 +1,28 @@
+import faker, { fake } from 'faker';
+import UserManager from '../../src/models/managers/UserManager';
+import User from '../../src/models/schemas/User';
+
+export default async function createUser(data = {}) {
+
+    const formatDate = (date: Date) => {
+        const day = date.getDate();
+        const month = date.getMonth();
+        const year = date.getFullYear();
+
+        return `${day}/${month}/${year}`
+    }
+
+    const dataFake = {
+        user_id: faker.internet.userName(),
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        whatsapp: faker.phone.phoneNumber(),
+        password: faker.internet.password(),
+        city: faker.address.city(),
+        birthday: formatDate(faker.date.past()),
+        avatar_id: faker.random.number({ min: 0, max: 12 })
+    }
+
+    const user = new User(dataFake.user_id);
+    await user.setBody({...dataFake, ...data});
+}
