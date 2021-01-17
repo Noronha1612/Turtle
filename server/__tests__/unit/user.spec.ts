cf2a1c00637fb0bcc80e6d6603d4bfeb82771018
NoronhaGCP <inc.691@gmail.com>
2021-01-17 00:34:14 -0300

    test: setBody unit method on User

--- server/__tests__/unit/user.spec.ts
+++ server/__tests__/unit/user.spec.ts
@@ -3,6 +3,7 @@ import User from '../../src/models/schemas/User';
 import createUser from '../utils/createUser';
 
 describe('users', () => {
+
     beforeEach(async () => {
         await db.migrate.latest();
     });
@@ -11,14 +12,44 @@ describe('users', () => {
         await db.migrate.rollback();
     });
 
-    it('should create a session token', async () => {
-        const user = await createUser({ user_id: 'noronha123' });
+    it('should setBody if user_id is registered in DB', async () => {
+        await createUser({user_id: 'noronha123'});
 
-        const token = user.createSessionToken();
+        const user = new User('noronha123');
 
-        expect(token).not.toBeUndefined();
-        expect(token).toMatch(/^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/);
+        await user.setBody();
+
+        expect(user.getBody()).not.toBeUndefined();
+        expect(user.getBody()?.name).not.toBeUndefined();
     });
 
-    
+    it('should not setBody if user_id is not registered in DB', async () => {
+        const user = new User('noronha123');
+
+        try {
+            await user.setBody();
+
+            expect(user.getBody()).toBeUndefined();
+        } catch(err) {
+            expect(err).not.toBeUndefined();
+        }
+    });
+
+    it('should setBody if data is valid', async () => {
+        const user = new User('noronha123');
+        await user.setBody(true, {
+            user_id: 'noronha',
+            avatar_id: 12,
+            birthday: '16/12/2002',
+            city: 'Recife',
+            email: 'inc.691@gmail.com',
+            name: 'Noronha',
+            password: '306090120',
+            whatsapp: '81983167399'
+        });
+
+        expect(user.getBody()).not.toBeUndefined();
+        expect(user.getBody()?.name).not.toBeUndefined();
+        expect(user.getBody()?.name).toBe('Noronha');
+    });
 })
