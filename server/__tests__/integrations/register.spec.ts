462f4eab01f861f0d8aab2d8a3fc5c33764cf31d
NoronhaGCP <inc.691@gmail.com>
2021-01-23 00:48:40 -0300

    test: register user route

--- server/__tests__/integrations/register.spec.ts
+++ server/__tests__/integrations/register.spec.ts
@@ -2,6 +2,7 @@ import truncate from "../utils/truncate";
 import request from 'supertest';
 import app from "../../src/app";
 import ResponseCodes from "../../src/interfaces/responseCodes";
+import createUser from "../utils/createUser";
 
 describe('register user', () => {
 
@@ -35,5 +36,124 @@ describe('register user', () => {
 
         expect(response.body.token).toMatch(tokenRegex);
         expect(response.status).toBe(ResponseCodes.CREATED);
+    });
+
+
+
+    it('should not create an user if passed invalid email', async () => {
+        const response = await request(app)
+            .post('/users')
+            .send({
+                user_id: 'g_noronhe-se',
+                first_name: 'Gabriel',
+                last_name: 'Noronha',
+                whatsapp: '81987654321',
+                email: 'teste@.com',
+                password: 'senhaSecreta',
+                confirm_password: 'senhaSecreta',
+                city: 'Recife',
+                birthday: '16/12/2002',
+                avatar_id: 12
+        });
+
+        expect(response.status).toBe(ResponseCodes.BAD_REQUEST);
+        expect(response.body.message).toBe('Invalid email');
+        expect(response.body.error).toBe(true);
+    });
+
+
+
+    it('should not create an user if passed password too short', async () => {
+        const response = await request(app)
+            .post('/users')
+            .send({
+                user_id: 'g_noronhe-se',
+                first_name: 'Gabriel',
+                last_name: 'Noronha',
+                whatsapp: '81987654321',
+                email: 'teste@gmail.com',
+                password: 'se',
+                confirm_password: 'se',
+                city: 'Recife',
+                birthday: '16/12/2002',
+                avatar_id: 12
+        });
+
+        expect(response.status).toBe(ResponseCodes.BAD_REQUEST);
+        expect(response.body.message).toBe('Password too short');
+        expect(response.body.error).toBe(true);
+    });
+
+
+
+    it('should not create an user if passed passwords that do not match', async () => {
+        const response = await request(app)
+            .post('/users')
+            .send({
+                user_id: 'g_noronhe-se',
+                first_name: 'Gabriel',
+                last_name: 'Noronha',
+                whatsapp: '81987654321',
+                email: 'teste@gmail.com',
+                password: 'senha1',
+                confirm_password: 'senha2',
+                city: 'Recife',
+                birthday: '16/12/2002',
+                avatar_id: 12
+        });
+
+        expect(response.status).toBe(ResponseCodes.BAD_REQUEST);
+        expect(response.body.message).toBe('Password and confirm password do not match');
+        expect(response.body.error).toBe(true);
+    });
+
+
+
+    it('should not create an user if passed an email that is already registered', async () => {
+        await createUser({ email: 'teste@gmail.com' });
+
+        const response = await request(app)
+            .post('/users')
+            .send({
+                user_id: 'g_noronhe-se',
+                first_name: 'Gabriel',
+                last_name: 'Noronha',
+                whatsapp: '81987654321',
+                email: 'teste@gmail.com',
+                password: 'se',
+                confirm_password: 'se',
+                city: 'Recife',
+                birthday: '16/12/2002',
+                avatar_id: 12
+        });
+
+        expect(response.status).toBe(ResponseCodes.FORBIDDEN);
+        expect(response.body.message).toBe('Email already exist');
+        expect(response.body.error).toBe(true);
+    });
+
+
+
+    it('should not create an user if passed an id that is already registered', async () => {
+        await createUser({ user_id: 'g_noronhe-se' });
+
+        const response = await request(app)
+            .post('/users')
+            .send({
+                user_id: 'g_noronhe-se',
+                first_name: 'Gabriel',
+                last_name: 'Noronha',
+                whatsapp: '81987654321',
+                email: 'teste@gmail.com',
+                password: 'se',
+                confirm_password: 'se',
+                city: 'Recife',
+                birthday: '16/12/2002',
+                avatar_id: 12
+        });
+
+        expect(response.status).toBe(ResponseCodes.FORBIDDEN);
+        expect(response.body.message).toBe('UserID already exist');
+        expect(response.body.error).toBe(true);
     });
 });
