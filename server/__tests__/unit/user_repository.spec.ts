214a79cbda5bea4a66c3314cd96345838cea4dcd
NoronhaGCP <inc.691@gmail.com>
2021-01-23 00:48:12 -0300

    test: create user_repository to test search

---
+++ server/__tests__/unit/user_repository.spec.ts
@@ -0,0 +1,42 @@
+import ResponseCodes from "../../src/interfaces/responseCodes";
+import UserRepository from "../../src/models/repositories/UserRepository";
+import createUser from "../utils/createUser";
+import truncate from "../utils/truncate";
+
+const userRepository = new UserRepository;
+
+describe('user_repository', () => {
+    beforeEach(async () => {
+        await truncate();
+    });
+    
+    afterEach(async () => {
+        await truncate();
+    });
+
+
+
+    it('should search all users', async () => {
+
+        await createUser();
+        await createUser({ first_name: 'Noronha' });
+        await createUser();
+
+        const allUsers = await userRepository.indexAllUsers();
+
+        expect(allUsers.length).toBe(3);
+        expect(allUsers[1].first_name).toBe('Noronha');
+    });
+
+
+
+    it('should get an user by email if email is registered', async() => {
+        await createUser({ email: 'teste@email.com' });
+
+        const userResponse = await userRepository.getUserByEmail('teste@email.com');
+        
+        expect(userResponse.data).not.toBeUndefined();
+        expect(userResponse.data?.email).toBe('teste@email.com');
+        expect(userResponse.code).toBe(ResponseCodes.OK);
+    });
+});
